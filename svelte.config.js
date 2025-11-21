615ddce9192b9aa58b6050ee316f9abeb659d523
Frido Emans <frido@emans.co>
2025-11-21 17:45:55 +0100

    Prepare for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
-// For Vercel deployment, uncomment the line below and comment out adapter-node:
-// import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-node';
+// For Vercel deployment:
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// adapter-node options for Docker deployment
-			out: 'build'
-		}),
+		adapter: adapter(),
 		alias: {
 			$lib: './src/lib'
 		}
